daeb2bed2e227f928d78cd0b62dab8a3ec506c6e
Gabriel-S-Machado <112127475+Gabriel-S-Machado@users.noreply.github.com>
2023-07-02 20:21:03 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+                //Lazy.js
+    //funções
+
+    function logg(a){
+        console.log(a)
+    }
+    
+    function verObj(objeto, executar){
+        Object.entries(objeto).forEach(([key, value])=>(
+            executar
+        ))
+    }
+    
+    //vars
+
+    const x = "X";
+    const marca = "------------------";
+    const pular = (pulos) => {let ns = ""; while(pulos !== 0){ns = ns + "\n"; pulos-- } return ns ;};
+
+                //Codigo
+    
+logg("a")
+                //Planejamento
+
+    //Fazer e atualizar Fichas automaticamente.
+    //Criar novas Fichas automaticamente
+    //Mudar as Cores de acordo com o tipo de Ficha
+    //derrepente colocar uma PNG do lado do Titulo do Site
+    
